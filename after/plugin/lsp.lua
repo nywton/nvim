e8193d5c6e3039c43453f5c9507fc37a8c70d9b8
Nywton Barros <nywton.barros@gmail.com>
2023-09-17 20:55:32 -0300

    fixes ruby-lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -9,49 +9,6 @@ lsp.ensure_installed({
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
-require'lspconfig'.ruby_ls.setup{}
-
-local lspconfig = require("lspconfig")
-local configs = require("lspconfig.configs")
-local util = require("lspconfig.util")
-
-if not configs.ruby_lsp then
-	local enabled_features = {
-		"documentHighlights",
-		"documentSymbols",
-		"foldingRanges",
-		"selectionRanges",
-		"semanticHighlighting",
-		"formatting",
-		"codeActions",
-	}
-
-	configs.ruby_lsp = {
-		default_config = {
-			cmd = { "bundle", "exec", "ruby-lsp" },
-			filetypes = { "ruby" },
-			root_dir = util.root_pattern("Gemfile", ".git"),
-			init_options = {
-				enabledFeatures = enabled_features,
-			},
-			settings = {},
-		},
-		commands = {
-			FormatRuby = {
-				function()
-					vim.lsp.buf.format({
-						name = "ruby_lsp",
-						async = true,
-					})
-				end,
-				description = "Format using ruby-lsp",
-			},
-		},
-	}
-end
-
-lspconfig.ruby_lsp.setup({ on_attach = on_attach, capabilities = capabilities })
-
 lsp.on_attach(function(client, bufnr)
 	local opts = {buffer = bufnr, remap = false}
 
@@ -69,5 +26,53 @@ lsp.on_attach(function(client, bufnr)
 
   lsp.default_keymaps({buffer = bufnr})
 end)
+
+-- textDocument/diagnostic support until 0.10.0 is released
+_timers = {}
+local function setup_diagnostics(client, buffer)
+  if require("vim.lsp.diagnostic")._enable then
+    return
+  end
+
+  local diagnostic_handler = function()
+    local params = vim.lsp.util.make_text_document_params(buffer)
+    client.request("textDocument/diagnostic", { textDocument = params }, function(err, result)
+      if err then
+        local err_msg = string.format("diagnostics error - %s", vim.inspect(err))
+        vim.lsp.log.error(err_msg)
+      end
+      if not result then
+        return
+      end
+      vim.lsp.diagnostic.on_publish_diagnostics(
+        nil,
+        vim.tbl_extend("keep", params, { diagnostics = result.items }),
+        { client_id = client.id }
+      )
+    end)
+  end
+
+  diagnostic_handler() -- to request diagnostics on buffer when first attaching
+
+  vim.api.nvim_buf_attach(buffer, false, {
+    on_lines = function()
+      if _timers[buffer] then
+        vim.fn.timer_stop(_timers[buffer])
+      end
+      _timers[buffer] = vim.fn.timer_start(200, diagnostic_handler)
+    end,
+    on_detach = function()
+      if _timers[buffer] then
+        vim.fn.timer_stop(_timers[buffer])
+      end
+    end,
+  })
+end
+
+require("lspconfig").ruby_ls.setup({
+  on_attach = function(client, buffer)
+    setup_diagnostics(client, buffer)
+  end,
+})
 
 lsp.setup()
