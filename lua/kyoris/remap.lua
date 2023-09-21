5bb5b68d96698fc05fab511daf2a74cc186020a2
Nywton Barros <nywton.barros@vagas.com.br>
2023-09-21 15:10:44 -0300

    add remaps shortcut

--- lua/kyoris/remap.lua
+++ lua/kyoris/remap.lua
@@ -40,4 +40,4 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/kyoris/packer.lua<CR>");
-
+vim.keymap.set("n", "<leader>km", "<cmd>e ~/.config/nvim/lua/kyoris/remap.lua<CR>");
