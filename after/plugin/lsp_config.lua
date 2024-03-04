ca36465950ed5a51540ba6d01d856d9726799b55
Nywton Barros <nywton.barros@gmail.com>
2024-03-03 22:56:12 -0300

    adds mason integration to lsp_zero config

--- after/plugin/lsp_config.lua
+++ after/plugin/lsp_config.lua
@@ -2,7 +2,8 @@ local lsp = require('lsp-zero')
 
 lsp.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
-  lsp.default_keymaps({buffer = bufnr})
+  lsp.default_keymaps({ buffer = bufnr })
+  vim.keymap.set('n', '<leader><leader>', vim.lsp.buf.format)
 end)
 
 -- to learn how to use mason.nvim with lsp-zero
@@ -12,7 +13,7 @@ require('mason-lspconfig').setup({
   handlers = {
     lsp.default_setup,
 
-    -- Install lsp gem by running: 
+    -- Install lsp gem by running:
     -- $ gem install ruby-lsp
     ruby_ls = function()
       require('lspconfig').ruby_ls.setup({
@@ -28,7 +29,41 @@ require('mason-lspconfig').setup({
   },
 })
 
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
 
+cmp.setup({
+  sources = {
+    { name = 'nvim_lsp' },
+  },
+  mapping = {
+    ['<Tab>'] = cmp_action.tab_complete(),
+    ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+    ['<C-y>'] = cmp.mapping.confirm({ select = false }),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<Up>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+    ['<Down>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
+    ['<C-p>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item({ behavior = 'insert' })
+      else
+        cmp.complete()
+      end
+    end),
+    ['<C-n>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_next_item({ behavior = 'insert' })
+      else
+        cmp.complete()
+      end
+    end),
+  },
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+})
 
 
 -- -- Keybinds
