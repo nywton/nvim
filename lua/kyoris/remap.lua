c7badaf537b05b7005a8a8a157b0e419111b6a2d
Nywton Barros <nywton.barros@gmail.com>
2024-11-03 14:02:02 -0300

    remaps

--- lua/kyoris/remap.lua
+++ lua/kyoris/remap.lua
@@ -18,6 +18,10 @@ vim.keymap.set("n", "<Right>", "<Nop>")
 vim.keymap.set("n", "<Down>", "<Nop>")
 vim.keymap.set("n", "<Left>", "<Nop>")
 
+-- Center cursor on jump back and forward
+vim.keymap.set("n", "<C-o>", "<C-o>zz") -- Go back in jump list and center
+vim.keymap.set("n", "<C-i>", "<C-i>zz") -- Go forward in jump list and center
+
 -- move code using visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
@@ -28,6 +32,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "G", "m`Gzz")
 
 -- remap leader p to void register (does not replace the previous yank)
 vim.keymap.set("x", "<leader>p", "\"_dP")
