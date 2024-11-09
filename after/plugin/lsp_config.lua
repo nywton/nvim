9e25e10c922f1a944f3b2c7f87669937084f4fb6
Nywton Barros <nywton.barros@gmail.com>
2024-11-09 14:45:18 -0300

    Removing autohover

--- after/plugin/lsp_config.lua
+++ after/plugin/lsp_config.lua
@@ -1,11 +1,15 @@
 local mason = require("mason")
 local mason_lspconfig = require("mason-lspconfig")
 local lspconfig = require("lspconfig")
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
 mason.setup()
 mason_lspconfig.setup({
   ensure_installed = { "html", "cssls", "tailwindcss", "gopls", "ruby_lsp", "elixirls" },
 })
+
+-- Capabilities for enhanced LSP completion
+local capabilities = cmp_nvim_lsp.default_capabilities()
 
 -- Setup LSP servers with Mason-LSPConfig
 mason_lspconfig.setup_handlers({
@@ -24,14 +28,6 @@ mason_lspconfig.setup_handlers({
         end
 
         -- Automatic hover documentation when available
-        vim.api.nvim_create_autocmd("CursorHold", {
-          buffer = bufnr,
-          callback = function()
-            vim.lsp.buf.hover()
-          end,
-        })
-
-        -- LSP keymaps
         local opts = { noremap = true, silent = true, buffer = bufnr }
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
         vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
